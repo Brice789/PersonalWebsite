f287ee9d45e58791b61ad34a005db95fa6cfcfed
Brice789 <107217757+Brice789@users.noreply.github.com>
2023-01-15 08:24:03 +0000

    --

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Brice Kireche',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Brice Kireche is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: 'oui.com',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
