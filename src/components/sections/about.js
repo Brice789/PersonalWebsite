c2206e96b4c3b7b883c6ac68ccf37003b94269e1
Brice789 <107217757+Brice789@users.noreply.github.com>
2023-01-15 08:30:21 +0000

    Organisation

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript', 'Python', 'React', 'C, C# C++', 'SQL', 'Docker'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,23 +135,23 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Brice and I enjoy creating things that live on the internet. My
+              interest in web development started back in 2020 when I decided to try editing a
+              website usefull for all — turns out hacking together a custom reblog button taught me
+              a lot about HTML &amp; CSS!
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
+              {/* <a href="https://us.mullenlowe.com/">an advertising</a>,{' '} */}
+              <a href="https://starry.com/">a start-up, Staar witch is outgoing </a>,{' '}
+              <a href="https://group.bnpparibas">a huge corporation</a>, and{' '}
+              {/* <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
               main focus these days is building accessible, inclusive products and digital
               experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              clients. */}
             </p>
-
+            {/* 
             <p>
               I also recently{' '}
               <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
@@ -159,7 +159,7 @@ const About = () => {
               </a>{' '}
               that covers everything you need to build a web app with the Spotify API using Node
               &amp; React.
-            </p>
+            </p> */}
 
             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
